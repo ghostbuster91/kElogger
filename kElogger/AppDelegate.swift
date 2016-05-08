c8c62c52e6051159a37e1a6ec0e6293aeb9f55b2
ghostbuster91 <kghost0@gmail.com>
2016-05-08 23:00:33 +0200

    allow user register and login via email

--- kElogger/AppDelegate.swift
+++ kElogger/AppDelegate.swift
@@ -3,11 +3,14 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    private let baseBackend = Firebase(url: "https://luminous-heat-7872.firebaseio.com")
+    
     @IBOutlet weak var githubTokenInput: NSTextField!
     @IBOutlet weak var window: NSWindow!
     
-    private var backendRef:Firebase?
-
+    @IBOutlet weak var emailInput: NSTextField!
+    @IBOutlet weak var passwordInput: NSTextField!
+    
     @IBAction func helpAction(sender: AnyObject) {
         let urlString = NSURL(string: "https://github.com/settings/tokens")
         
@@ -15,43 +18,73 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @IBAction func loginAction(sender: AnyObject) {
-        loginUser(githubTokenInput.stringValue)
+        loginUserWithGithub(githubTokenInput.stringValue)
     }
     
-    private func loginUser(githubToken: String){
-        let ref = Firebase(url: "https://luminous-heat-7872.firebaseio.com")
-        ref.authWithOAuthProvider("github", token:githubToken,
+    @IBAction func loginWithEmailAction(sender: AnyObject) {
+        loginUserWithPassword(emailInput.stringValue, password: passwordInput.stringValue)
+    }
+    @IBAction func registerAndLoginAction(sender: AnyObject) {
+        registerAndLoginUser(emailInput.stringValue, password: passwordInput.stringValue)
+    }
+    
+    private func loginUserWithGithub(githubToken: String){
+        baseBackend.authWithOAuthProvider("github", token:githubToken,
                                   withCompletionBlock: { error, authData in
                                     if error != nil {
                                         // There was an error during log in
                                         print("error during log in")
                                     } else {
-                                        self.window.close()
                                         let githubUsername = authData.providerData["username"]
-                                        let userStorage = ref.childByAppendingPath("users").childByAppendingPath(authData.uid)
-                                        self.onLoginSucced(userStorage, githubUsername: githubUsername as! String)
+                                        self.onLoginSucceed(authData.uid, username: githubUsername as! String)
                                     }
         })
     }
     
-    private func onLoginSucced(firebase: Firebase, githubUsername: String){
-        firebase.observeSingleEventOfType(.Value, withBlock: { snapshot in
-            let shouldCreateUser = (snapshot.value.objectForKey("full_name") as? String) == nil
+    private func registerAndLoginUser(email: String, password: String){
+        baseBackend.createUser(email, password: password,
+                       withValueCompletionBlock: { error, result in
+                        if error != nil {
+                            print("error during registering")
+                            // There was an error creating the account
+                        } else {
+                           self.loginUserWithPassword(email, password: password)
+                        }
+        })
+    }
+    
+    private func loginUserWithPassword(email: String, password: String){
+        baseBackend.authUser(email, password: password,
+                     withCompletionBlock: { error, authData in
+                        if error != nil {
+                            print("error during log in")
+                            // There was an error logging in to this account
+                        } else {
+                            self.onLoginSucceed(authData.uid, username: email)
+                        }
+        })
+    }
+    
+    private func onLoginSucceed(uid: String, username: String){
+        self.window.close()
+        let userBackend = self.baseBackend.childByAppendingPath("users").childByAppendingPath(uid)
+        userBackend.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            let shouldCreateUser = snapshot.value is NSNull
             var keystrokes = 0
             var mouseClicks = 0
             var mouseMoves = 0
             var mouseMovesDistance:Double = 0
             if shouldCreateUser {
                 print("creating user...")
-                let userData = ["full_name": githubUsername, "keystrokes": 0, "mouseClicks": 0, "mouseMoves": 0, "mouseMovesDistance": Double(0)]
-                firebase.setValue(userData)
+                let userData = ["full_name": username, "keystrokes": 0, "mouseClicks": 0, "mouseMoves": 0, "mouseMovesDistance": Double(0)]
+                userBackend.setValue(userData)
             }else{
                 keystrokes = snapshot.value.objectForKey("keystrokes") as! Int!
                 mouseClicks = snapshot.value.objectForKey("mouseClicks") as! Int!
                 mouseMoves = snapshot.value.objectForKey("mouseMoves") as! Int!
                 mouseMovesDistance = snapshot.value.objectForKey("mouseMovesDistance") as! Double!
             }
-            KeyListener(firebase: firebase, keystrokes: keystrokes,mouseClicks: mouseClicks, mouseMooved:mouseMoves,mouseMoovedDistance: mouseMovesDistance).start()
+            KeyListener(firebase: userBackend, keystrokes: keystrokes,mouseClicks: mouseClicks, mouseMooved:mouseMoves,mouseMoovedDistance: mouseMovesDistance).start()
         })
     }
     
